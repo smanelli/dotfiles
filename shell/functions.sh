8afd246a43e1a85b27a05d9cc663c61afb1b1905
Stefano Manelli <smanelli@gmail.com>
2020-09-23 12:26:28 +0200

    added prev function for pet snipping utility

--- shell/functions.sh
+++ shell/functions.sh
@@ -28,3 +28,8 @@ there="$HOME/.shell.here"
 there() {
     cd "$(readlink "${there}")"
 }
+
+function prev() {
+  PREV=$(echo `history | tail -n2 | head -n1` | sed 's/[0-9]* //')
+  sh -c "pet new `printf %q "$PREV"`"
+}
